cb53f947e4de22748a60eccb6547fb5471a200f8
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2022-04-27 10:53:13 -0300

    :up: Estrutura iniciais

---
+++ src/A0001-type-annotation/A0001-type-annotation.ts
@@ -0,0 +1,7 @@
+/*eslint-disable */
+// Tipos bpasicos (aqui ocrre inferência de tipos)
+let nome: string = 'Tawan'; // Qualquer tipo de strings; '' "" ``
+let idade: number = 30; // 10, 1.57, -5.55, 0xf00f, 0b1010, 0o7744
+let adulto: boolean = true; // true ou false
+let simbolo: symbol = Symbol('qualquer-symbol'); // symbol
+let big: bigint = 10n; // bigint
